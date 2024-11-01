9afc7dcf0c14223a51a8bd2db1be3ab79aba8259
AdrianMares <adrianmares.dev@gmail.com>
2024-10-31 22:50:37 -0400

    light mode icon toggle

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 //Light and Dark mode toggle
 const lightModeBtn = document.getElementById('light-mode-toggle');
+const moonIcon = document.getElementById('moon-icon');
+const sunIcon = document.getElementById('sun-icon');
 const body = document.body;
 
 lightModeBtn.addEventListener('click', lightModeToggle);
 
-function lightModeToggle () {
+function lightModeToggle() {
     body.classList.toggle('light-mode');
+
+    //Toggle correct icon
+    if (body.classList.contains('light-mode')) {
+        moonIcon.classList.remove('hidden');
+        sunIcon.classList.add('hidden');
+    } else {
+        moonIcon.classList.add('hidden');
+        sunIcon.classList.remove('hidden');
+    }
 }
